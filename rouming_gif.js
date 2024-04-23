46ad08c998bee9e037f7cdd37cdc6a1e82ef5727
JMusilek <jmusilek@gk-software.com>
2024-04-23 14:16:28 +0200

    Gif - move comments to the sidebar, set max width and height, identify control panels, comment out the log messages

--- rouming_gif.js
+++ rouming_gif.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Control GIFs with arrow key
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Opens link with text "&gt;&gt;" using the right arrow key.
 // @author       You
 // @match        https://www.rouming.cz/roumingGIF.php*
@@ -11,6 +11,50 @@
 (function() {
     'use strict';
 
+    // identifying the Control panels (should be two of them)
+    let controls = document.getElementsByClassName('control');
+    if (controls && controls.length === 2) {
+        console.log('Found Two control panels');
+        controls[0].setAttribute('id', 'control-panel-up');
+        controls[1].setAttribute('id', 'control-panel-down');
+    }
+    else {
+        console.log('Not found expected control panels, found: ', controls ?? controls.length);
+    }
+
+    // identifying the tables
+    let tables = document.querySelectorAll('table');
+    console.log('Tables found: ', tables ?? tables.length);
+    if (tables) {
+        tables[1].setAttribute('id', 'table-video');
+        tables[2].setAttribute('id', 'table-comments');
+        console.log('Identified tables: ', tables[1], tables[2])
+    }
+
+    //move comments from wrapper -> sidebar
+    let middle = document.querySelector('.middle');
+    console.log('Middle:', middle);
+    let sidebar = document.querySelector('.sidebar');
+    if (middle && sidebar) {
+        console.log('Found middle and sidebar:', middle, sidebar);
+        let middleComments = document.getElementById('table-comments');
+        if (middleComments) {
+            console.log('Found middleComment:', middleComments);
+            sidebar.appendChild(middleComments);
+        }
+    }
+
+    // Video eleemtn max height:
+    window.onload = function() {
+        let video = document.getElementById('video');
+        if (video) {
+            video.style.maxHeight   = '900px';
+            video.style.maxWidth    = '900px';
+            video.loop = false;
+        }
+    };
+
+
     // Find all links on the page
     let links = document.querySelectorAll('a');
 
@@ -19,14 +63,14 @@
       function(link) {
         // If the link contains the text "&gt;&gt;"
         if (link.textContent.includes('&gt;&gt;') || link.textContent.includes('>>')) {
-            console.log('Found:', link);
+            //console.log('Found:', link);
             link.classList.add('next-gif');
-            console.log(link);
+            //console.log(link);
         }
         if (link.textContent.includes('&lt;&lt;') || link.textContent.includes('<<')) {
-            console.log('Found:', link);
+            //console.log('Found:', link);
             link.classList.add('prev-gif');
-            console.log(link);
+            //console.log(link);
         }
     });
 
@@ -45,7 +89,7 @@
             // up arrow
             console.log('^^');
             if (nextGifLink) {
-                console.log(nextGifLink.href);
+                //console.log(nextGifLink.href);
                 // Increase volume by 10%
                 video.volume = video.volume + 0.1;
             }
@@ -54,21 +98,21 @@
             // down arrow
             console.log('ˇˇ');
             if (nextGifLink) {
-                console.log(nextGifLink.href);
+                //console.log(nextGifLink.href);
                 // Mute the volume
                 video.volume = 0;
             }
         }
         else if (e.keyCode == '37') {
             // left arrow
-            console.log('<<');
+            //console.log('<<');
             if (prevGifLink) {
-                console.log(prevGifLink.href);
+                //console.log(prevGifLink.href);
 
                 window.location.href = prevGifLink.href;
             }
             else {
-                console.log('I am at the beginning.');
+                //console.log('I am at the beginning.');
                 window.location.href = 'https://www.rouming.cz/';
             }
         }
@@ -76,7 +120,7 @@
             // right arrow
             console.log('>>');
             if (nextGifLink) {
-                console.log(nextGifLink.href);
+                //console.log(nextGifLink.href);
 
                 window.location.href = nextGifLink.href;
             }
